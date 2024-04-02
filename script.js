4e602c481c31f0837d72150ff7f7420026a779cd
DESKTOP-KA6S9B7\SUCCESS <mdsajidurrahman375@gmail.com>
2024-04-02 19:43:13 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function imageslider(image){
+    document.querySelector('.starbucks').src = image
+}
+
+function changecolor(color){
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
